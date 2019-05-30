e10646c652695192196bd43bd03b65d906be1aec
Glen <fordp42@gmail.com>
2019-05-30 21:36:50 +1000

    Made the code more readable.

--- lib/pages/roman_to_decimal.dart
+++ lib/pages/roman_to_decimal.dart
@@ -42,6 +42,110 @@ class _RomanToDecimalState extends State<RomanToDecimal> {
     });
   }
 
+  Widget textboxEntryArea() {
+    return new TextField(
+      textInputAction: TextInputAction.go,
+      decoration: new InputDecoration(
+        labelText: "Enter a roman numeral",
+        fillColor: Colors.white,
+        border: new OutlineInputBorder(
+          borderRadius: new BorderRadius.circular(25.0),
+          borderSide: new BorderSide(),
+        ),
+      ),
+      controller: myController,
+      onChanged: (v) => setState(() {
+            //_roman = v;
+          }),
+      keyboardType: TextInputType.number,
+      style: new TextStyle(fontFamily: "Arvo"),
+    );
+  }
+
+  Widget conversionResultAreaText() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: new Text(
+        "Decimal Value: ",
+        style: TextStyle(
+          fontSize: 25,
+          color: hexToColor("#F2A03D"),
+          // fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget conversionResultAreaResult() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: new Text(
+        _dec,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+          color: Colors.redAccent,
+        ),
+      ),
+    );
+  }
+
+  Widget convertButton() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Builder(
+        builder: (context) {
+          return RaisedButton(
+            onPressed: () => submitConvertNumber(context),
+            color: Colors.lightGreen,
+            child: Text(
+              'Convert',
+              style: TextStyle(color: Colors.white),
+            ),
+            shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(25.0)),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget clearButton() {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Builder(
+        builder: (context) {
+          return RaisedButton(
+            onPressed: () => submitClearNumber(context),
+            color: Colors.lightBlueAccent,
+            child: Text(
+              'Clear',
+              style: TextStyle(color: Colors.white),
+            ),
+            shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(25.0)),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buttonArea() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        convertButton(),
+        clearButton(),
+      ],
+    );
+  }
+
+  Widget paddingEdgeInsetsTop(var number) {
+    return new Padding(
+      padding: EdgeInsets.only(top: number),
+    );
+  }
+
   Widget createConversionArea(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(30.0),
@@ -50,92 +154,17 @@ class _RomanToDecimalState extends State<RomanToDecimal> {
         child: new Center(
           child: new Column(
             children: [
-              new Padding(
-                padding: EdgeInsets.only(top: 40.0),
-              ),
+              paddingEdgeInsetsTop(40.0),
               new Text(
                 'Enter a Roman Numeral.',
                 style:
                     new TextStyle(color: hexToColor("#F2A03D"), fontSize: 25.0),
               ),
-              new Padding(
-                padding: EdgeInsets.only(top: 20.0),
-              ),
-              new TextField(
-                decoration: new InputDecoration(
-                  labelText: "Enter a roman numeral",
-                  fillColor: Colors.white,
-                  border: new OutlineInputBorder(
-                    borderRadius: new BorderRadius.circular(25.0),
-                    borderSide: new BorderSide(),
-                  ),
-                ),
-                controller: myController,
-                onChanged: (v) => setState(() {
-                      // _dec = v;
-                    }),
-                // keyboardType: TextInputType.number,
-                style: new TextStyle(fontFamily: "Arvo"),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: new Text(
-                  "Decimal Number: ",
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: hexToColor("#F2A03D"),
-                    // fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: new Text(
-                  _dec,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    color: Colors.redAccent,
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Builder(
-                      builder: (context) {
-                        return RaisedButton(
-                          onPressed: () => submitConvertNumber(context),
-                          color: Colors.lightGreen,
-                          child: Text(
-                            'Convert',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(25.0)),
-                        );
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Builder(
-                      builder: (context) {
-                        return RaisedButton(
-                          onPressed: () => submitClearNumber(context),
-                          color: Colors.lightBlueAccent,
-                          child: Text(
-                            'Clear',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(25.0)),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
+              paddingEdgeInsetsTop(20.0),
+              textboxEntryArea(),
+              conversionResultAreaText(),
+              conversionResultAreaResult(),
+              buttonArea(),
             ],
           ),
         ),
